5bc81abdc51d612be0364c05acf0ab062f7767b6
Clint <cahenrichs@gmail.com>
2026-05-14 15:40:15 -0500

    created an rss feed parser

--- src/lib/rss.ts
+++ src/lib/rss.ts
@@ -21,7 +21,49 @@ export async function fetchFeed(feedURL: string) {
         }
     });
     const xml = await response.text();
-    const parser = new XMLParser();
+    const parser = new XMLParser({processEntities: false});
     const jsonObj = parser.parse(xml);
+
+    if (!jsonObj.rss || !jsonObj.rss.channel) {
+        throw new Error("Invalid RSS feed");
+    }
+    const channel = jsonObj.rss.channel;
+    
+   if (typeof channel.title !== "string" || typeof channel.link !== "string" || typeof channel.description !== "string") {
+    throw new Error("Invalid RSS feed: Missing required channel properties");
+   }
+
+   let rawItems: any[] = [];
+   if (channel.item) {
+   if (Array.isArray(channel.item)) {
+       rawItems = channel.item;
+   } else {
+    rawItems = [channel.item];
+   }
+}
+const items: RSSItem[] = [];
+for (const item of rawItems) {
+    if (
+        typeof item.title !== "string" ||
+        typeof item.link !== "string" ||
+        typeof item.description !== "string" ||
+        typeof item.pubDate !== "string"
+    ) {
+        continue; 
+    }
+
+    items.push({
+        title: item.title,
+        link: item.link,
+        description: item.description,
+        pubDate: item.pubDate
+    });     
+}
+return {
+        title: channel.title,
+        link: channel.link,
+        description: channel.description,
+        items: items
+    };
 
 }
